959df41d097b196fee6aa5e49ec4f1858a8d21d1
gustavo <bocettodigital@gmail.com>
2022-01-11 23:12:31 -0500

    Back funcionando ok

--- models/enum/types.ts
+++ models/enum/types.ts
@@ -4,14 +4,13 @@ const typesEnums = gql`
   # Se definen los enum fuera de cada tipo, y sun contenido debe ir en mayúscula por asi lo define GraphQl
   enum Enum_Rol {
     ESTUDIANTE
-    LIDER
     ADMINISTRADOR
   }
 
   enum Enum_StatusUsers {
     PENDIENTE
-    AUTORIZADO
-    NO_AUTORIZADO
+    ACTIVO
+    INACTIVO
   }
 `;
 
